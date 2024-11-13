49f28cc1b9621f281f120ee5ef6a78f679ecc417
Rob.a <rob@MacBook-Pro.local>
2024-11-12 21:53:11 -0700

    edit Vite config about.html error

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: "index.html", // Main entry point
-        about: "about.html" // Second entry point
+        privacy: "privacy-policy.html" // Second entry point
       }
     }
   }
